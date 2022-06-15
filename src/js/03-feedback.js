a063842078167846075f1dc83c0f1652baaab7ce
Ewa Domańska <ewadomanska0510@gmail.com>
2022-06-15 13:21:16 +0200

    Part od task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+let form = document.querySelector(".feedback-form");
 
+form.addEventListener("input", (e) => {
+  let oldObject = localStorage.getItem("feedback-form-state");
+  let email = oldObject ? JSON.parse(oldObject).email : "";
+  let message = oldObject ? JSON.parse(oldObject).message : "";
+  if (e.target.name === "email") {
+    email = e.target.value;
+  } else {
+    message = e.target.value;
+  }
+
+  const object = {
+    "email" : email,
+    "message" : message
+  }
+  localStorage.setItem("feedback-form-state", JSON.stringify(object));
+})
