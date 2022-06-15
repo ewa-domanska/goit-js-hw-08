1d139df253979e5e0a83b1f4bb4249a8deccdc15
Ewa Domańska <ewadomanska0510@gmail.com>
2022-06-15 12:24:47 +0200

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = Number(localStorage.getItem("videoplayer-current-time"))
+player.setCurrentTime(currentTime)
+
+const saveTime = function (currentTime) {
+  localStorage.setItem("videoplayer-current-time", currentTime.seconds)
+};
+
+player.on('timeupdate', throttle(saveTime, 1000));
